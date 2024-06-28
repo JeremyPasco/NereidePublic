d18a47a1424957928c50104ec4b809932d04413e
Jeremy Pasco <jeremypasco37@gmail.com>
2024-06-28 16:59:12 +0200

    Multithreading ex 2

---
+++ src/main/java/org/example/ex7/start2/BigDataOps.java
@@ -0,0 +1,15 @@
+package org.example.ex7.start2;
+
+import java.util.Random;
+
+public class BigDataOps {
+    public static int blackBox() {
+        byte[] data = new byte[50 * 1024 * 1024];
+        new Random().nextBytes(data);
+        if(data[0] > 10) {
+            return new Random().nextInt(0, 9);
+        } else {
+            return new Random().nextInt(0, 8);
+        }
+    }
+}
---
+++ src/main/java/org/example/ex7/start2/MultiThreading.java
@@ -0,0 +1,43 @@
+package org.example.ex7.start2;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MultiThreading {
+
+    public static void main(String[] args) {
+        int nbThreads = 1000;
+        int nbTasks = 1000;
+
+        long startTime = System.currentTimeMillis();
+        try (ExecutorService executor = Executors.newFixedThreadPool(nbThreads)) {
+
+            CompletableFuture<?>[] futures = new CompletableFuture[nbTasks];
+
+            IntStream.range(0, nbTasks).forEach(i ->
+                    futures[i] = CompletableFuture.supplyAsync(BigDataOps::blackBox, executor));
+
+            CompletableFuture.allOf(futures).join();
+
+            String combined = Stream.of(futures)
+                    .map(f -> {
+                        try {
+                            return String.valueOf(f.get());
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.joining());
+
+            System.out.println(combined);
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
+    }
+}
