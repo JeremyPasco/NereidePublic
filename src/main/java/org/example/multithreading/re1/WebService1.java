fb1709401f8f8621280fb209e25fc79caeac3bdd
Jeremy Pasco <jeremypasco37@gmail.com>
2024-06-28 14:23:33 +0200

    Multithreading ex 1

---
+++ src/main/java/org/example/multithreading/re1/WebService1.java
@@ -0,0 +1,8 @@
+package org.example.multithreading.re1;
+
+public class WebService1 {
+    public static int SendRequest() throws InterruptedException {
+        Thread.sleep(2000);
+        return 1;
+    }
+}
---
+++ src/main/java/org/example/multithreading/re1/WebService2.java
@@ -0,0 +1,8 @@
+package org.example.multithreading.re1;
+
+public class WebService2 {
+    public static int SendRequest() throws InterruptedException {
+        Thread.sleep(5000);
+        return 3;
+    }
+}
